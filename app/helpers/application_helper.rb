3c50e93986bb70a63a49d927ecc783bcb18fede2
Paulo Terenzi <pterenzi@wayoftheweb.net>
2010-09-17 23:04:16 -0300

    Tooltips and links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def tooltip(content, options = {}, html_options = {}, *parameters_for_method_reference)
+  html_options[:title] = options[:tooltip]
+  html_options[:class] = html_options[:class] || ‘tooltip’
+  content_tag("span", content, html_options)
+  end
 end
